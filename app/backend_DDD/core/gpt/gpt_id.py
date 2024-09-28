6c448f7d63e2e22d6467e7c20e84e37cfb35f4db
xBoy-09 <24100300@lums.edu.pk>
2024-09-28 17:26:28 +0500

    Changed assistant

--- app/backend_DDD/core/gpt/gpt_id.py
+++ app/backend_DDD/core/gpt/gpt_id.py
@@ -1,9 +1,9 @@
-lums_public_assitant_id = "asst_QmHUOt23cbf7zjN30jPVAHhV" # a1
-# lums_public_thread_id = 'thread_gyyciRSR2HMlqtV5go1p8cNt' # t1
-lums_public_thread_id = 'thread_NgnngAuKGnhjT1JkXKamLJQH' # t2
+# lums_public_assitant_id = "asst_QmHUOt23cbf7zjN30jPVAHhV" # a1
+# # lums_public_thread_id = 'thread_gyyciRSR2HMlqtV5go1p8cNt' # t1
+# lums_public_thread_id = 'thread_NgnngAuKGnhjT1JkXKamLJQH' # t2
 
 
-# lums_public_assitant_id = "asst_WD5aHU2OiuzPkj3lsXHDya8O"
-# lums_public_thread_id = "thread_MPLAOaaC61YaNRX2yCAz5Yu2" #a2  t2
+lums_public_assitant_id = "asst_WD5aHU2OiuzPkj3lsXHDya8O"
+lums_public_thread_id = "thread_MPLAOaaC61YaNRX2yCAz5Yu2" #a2  t1
 
 
